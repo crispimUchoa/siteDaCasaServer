523ce8231456a13e12efe773b0319a305738f647
crispimUchoa <francisco,crispim@aluno.uece.br>
2024-03-04 11:03:02 -0300

    feita as associações entre tasks e users

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,9 @@ import {Task} from './Task'
 import {User} from './User'
 import {List} from './List'
 
+Task.belongsToMany(User, {through: List})
+
+User.belongsToMany(Task, {through: List})
 
 export = {
   Task,
